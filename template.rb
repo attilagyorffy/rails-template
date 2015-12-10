c4ae917796fab013a6499360e6c75f3257c027bf
Attila Györffy <attila@attilagyorffy.com>
2015-12-10 01:39:30 +0100

    Add template

---
+++ template.rb
@@ -0,0 +1,39 @@
+# template.rb
+
+gem_group :development, :test do
+  gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails'
+end
+
+gem_group :development do
+  gem 'guard-livereload', '~> 2.4', require: false
+  gem 'guard-rspec', require: false
+  gem 'annotate'
+end
+
+gem_group :test do
+  gem 'capybara'
+end
+
+generator_config = %Q{
+  config.generators do |generators|
+    generators.helper       false
+    generators.javascripts  false
+    generators.stylesheets  false
+    generators.assets       false
+    generators.view_specs   false
+    generators.helper_specs false
+  end
+}
+
+environment generator_config
+
+run "bundle install"
+
+generate 'rspec:install'
+
+after_bundle do
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial Rails application' }
+end
